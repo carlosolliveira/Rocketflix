0325ca11311316491dd2f8f029efbd3afcd54069
Carlos <vxcarlosig@gmail.com>
2022-10-22 13:05:31 -0300

    Start main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// https://api.themoviedb.org/3/movie/63?api_key=95d050a1db3553d17c6e7fb1cb829e04
+
+const apiKey = "95d050a1db3553d17c6e7fb1cb829e04"
+const url = "https://api.themoviedb.org/3/movie/550?api_key="
+
+
+function randomNum(){
+    return Math.random()
+}
+
+function randommovie(){
+    console.log("Hi")
+}
