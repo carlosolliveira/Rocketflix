382c3702884b9cfab2fd0b6bccf030eb26ba7fd9
Carlos Oliveira <vxcarlosig@gmail.com>
2022-10-25 15:59:36 -0300

    remove keys api file js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// https://api.themoviedb.org/3/movie/63?api_key=95d050a1db3553d17c6e7fb1cb829e04
-
-const apiKey = "95d050a1db3553d17c6e7fb1cb829e04"
-const url = "https://api.themoviedb.org/3/movie/550?api_key="
 
 
 function randomNum(){
